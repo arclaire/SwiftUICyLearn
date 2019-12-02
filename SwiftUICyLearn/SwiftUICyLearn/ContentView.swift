982dc2f4af8d43c7d96869d8ff66dad0b05b497c
Lucy <yuki_lcy@yahoo.com>
2019-12-02 10:30:32 +0700

    first comm

---
+++ SwiftUICyLearn/SwiftUICyLearn/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUICyLearn
+//
+//  Created by Lucy on 02/12/19.
+//  Copyright © 2019 Lucy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
